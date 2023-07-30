0515e1a8a65c5aab23a6e7f82095df2dc4bc0a7e
ppoonk <101167225+ppoonk@users.noreply.github.com>
2023-07-30 12:52:27 +0800

    update 20230730

--- server/model/shell_model_text.go
+++ server/model/shell_model_text.go
@@ -159,12 +159,4 @@ iptables -t nat -F
 iptables -t mangle -F
 iptables -F
 iptables -X
-
-ip6tables -P INPUT ACCEPT
-ip6tables -P FORWARD ACCEPT
-ip6tables -P OUTPUT ACCEPT
-ip6tables -t nat -F
-ip6tables -t mangle -F
-ip6tables -F
-ip6tables -X
 `
